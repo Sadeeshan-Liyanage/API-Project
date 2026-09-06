7e9445fd3d2e21a64b50f0ae295c85ce7dcdfb34
sadeeshan <sadeeshanlk3@gmail.com>
2026-09-06 12:00:41 +0530

    update payment service class

--- src/main/java/com/example/api_project/service/PaymentService.java
+++ src/main/java/com/example/api_project/service/PaymentService.java
@@ -75,9 +75,7 @@ public class PaymentService {
         }
 
 
-        auditLogger.log("CREATE", "Payment", saved.getId(),
-                "order=" + order.getId() + " amount=" + saved.getAmount() + " method=" + saved.getMethod());
-        return saved;
+
     }
 
 
